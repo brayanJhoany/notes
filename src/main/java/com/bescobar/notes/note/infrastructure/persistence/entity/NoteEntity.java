cbf6829c4f007e5098e6e6232b47bc553f54bcc3
Brayan Escobar <bescobarwant@gmail.com>
2025-11-11 21:18:30 -0300

    fix: reorganizar importaciones

--- src/main/java/com/bescobar/notes/note/infrastructure/persistence/entity/NoteEntity.java
+++ src/main/java/com/bescobar/notes/note/infrastructure/persistence/entity/NoteEntity.java
@@ -1,15 +1,25 @@
 package com.bescobar.notes.note.infrastructure.persistence.entity;
 
+import java.time.LocalDateTime;
+
 import com.bescobar.notes.user.infrastructure.persistence.entity.UserEntity;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
--- src/main/java/com/bescobar/notes/note/infrastructure/persistence/repository/NoteJpaRepository.java
+++ src/main/java/com/bescobar/notes/note/infrastructure/persistence/repository/NoteJpaRepository.java
@@ -1,11 +1,12 @@
 package com.bescobar.notes.note.infrastructure.persistence.repository;
 
-import com.bescobar.notes.note.infrastructure.persistence.entity.NoteEntity;
-import com.bescobar.notes.user.infrastructure.persistence.entity.UserEntity;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.bescobar.notes.note.infrastructure.persistence.entity.NoteEntity;
+import com.bescobar.notes.user.infrastructure.persistence.entity.UserEntity;
 
 @Repository
 public interface NoteJpaRepository extends JpaRepository<NoteEntity, Long> {
